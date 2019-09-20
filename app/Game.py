c6eef86e7e4e41c5db07d6049272b2ec153a2a4c
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2019-09-20 12:38:34 +0100

    Add files via upload

---
+++ app/Game.py
@@ -0,0 +1,59 @@
+from random import choice
+from bs4 import BeautifulSoup
+from requests import get
+from app.Config import BASE_URL
+
+def start_game(QUOTES):
+  user_ans = ""
+  guesses = 4
+  ans = choice(QUOTES)
+
+  def get_clue_data(link):
+      ABOUT_URL = BASE_URL + link
+      about_req = get(ABOUT_URL)
+      about = BeautifulSoup(about_req.text, "html.parser")
+      return about
+  print("")
+  print("Guess who said the following quote: ")
+  print(ans["text"])
+
+  while user_ans != ans["author"] and guesses != 0:
+    print("Clues Left: " + str(guesses) + "/4" )
+    user_ans = input()
+    guesses -= 1
+    print("")
+
+    if(user_ans.lower() == ans["author"].lower()):
+      print("THATS CORRECT!")
+      break
+      
+    elif(guesses == 3):
+      clue = get_clue_data(ans["link"])
+      born = clue.find(class_="author-born-date").get_text()
+      print("First clue: The author was born " + born)
+
+    elif(guesses == 2):
+      clue = get_clue_data(ans["link"])
+      location = clue.find(class_="author-born-location").get_text()
+      print("Second clue: Was Born " + location)
+
+    elif(guesses == 1):
+      clue = get_clue_data(ans["link"])
+      first = clue.find(class_="author-title").get_text()[0]
+      second = clue.find(class_="author-title").get_text().split(" ")[1][0]
+      print("Last clue: Initials are " + first + second)
+
+    else:
+      print("Too bad, the answer was " + ans["author"])
+      break
+
+  again = ""
+  while again.lower() not in ("y", "yes", "n", "no"):
+    again = input("Would you like to play again? (y/n): ")
+    if again.lower() in ("y", "yes"):
+      return start_game(QUOTES)
+    else:
+      print("Sorry i didnt get that?")
+      print("")
+  
+  print("Thanks for playing!")
---
+++ main.py
@@ -0,0 +1,10 @@
+from app.Config import BASE_URL
+from app.Game import start_game
+from app.Scraper import get_quotes
+
+
+
+if __name__ == "__main__":
+  QUOTES = get_quotes(BASE_URL)
+  start_game(QUOTES)
+
